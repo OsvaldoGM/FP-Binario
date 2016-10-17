85d68ec9e191fc36fc4e30e8aef66715c4254c1f
Alumno <Alumno@328-26>
2016-10-17 12:22:07 -0500

    Binario

---
+++ src/fp/numerobinario/FPNumeroBinario.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fp.numerobinario;
+import java.util.*;
+/**
+ *
+ * @author Alumno
+ */
+public class FPNumeroBinario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public static int pedirNumero(){
+        int n;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Intrdouzca el numero que quiere convertir a binario ");
+        return n=entrada.nextInt();
+        /*
+        Random numAleatorio = new Random();
+        a = numAleatorio.nextInt();
+        */
+    }
+    public static int calcularBin(int n){
+        String r="";
+        int residuo=0;
+        while(n!=0){
+               residuo=n%2;
+               
+                }
+    }
+    public static void muestraBin(String b){
+        char cad[];
+        int a=0;
+        cad = b.toCharArray();
+        a = cad.length-1;
+        while(a>0){
+            b=b+cad[a];
+            a--;
+        }
+        System.out.println("El numero binario es :  "  +b);
+    }
+}
